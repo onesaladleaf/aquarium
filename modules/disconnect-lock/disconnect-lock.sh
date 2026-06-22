ec569347e16818e331c47b8519148128d4f2bc79
onesaladleaf <180638599+onesaladleaf@users.noreply.github.com>
2026-06-22 22:22:32 +0300

    Prettier disconnect-lock.sh

--- modules/disconnect-lock/disconnect-lock.sh
+++ modules/disconnect-lock/disconnect-lock.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-systemd-run --uid=1000 --gid=1000 --setenv=WAYLAND_DISPLAY="wayland-1" --setenv=XDG_RUNTIME_DIR="/run/user/1000" swaylock --color 1f1f28 --inside-color 1f1f28 --line-color 1f1f2
+systemd-run \
+    --uid=1000 --gid=1000 \
+    --setenv=WAYLAND_DISPLAY="wayland-1" \
+    --setenv=XDG_RUNTIME_DIR="/run/user/1000" \
+    swaylock --color 1f1f28 --inside-color 1f1f28 --line-color 1f1f2
